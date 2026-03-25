24c5ee40ee421d4f13fe75f6fd329f9dc81cf36f
tuhinSubhhr <tuhinsubhragiri6@gmail.com>
2026-03-25 17:02:05 +0530

    remove file

--- dom.js
+++
@@ -1,8 +0,0 @@
-//let h2 = document.querySelector("h2");
-//console.dir(h2.innerText);
-//h2.innerText = h2.innerText + "from tecno india students";
-
-
-let div = document.querySelector("div");
-div.style.backgroundColor = "green";
-div.style.fontSize = "28px"
--- firstjavascript.js
+++
@@ -1,9 +0,0 @@
-const product = {
-    fullName: "Parker pen",
-    price: 270,
-    rating: 4.5c
-}
-}
-
-
-console.log(product["rating"])
